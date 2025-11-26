e4b59c1cc3a27250de495fbddfee44e6e7b84e52
unwale <ndubrovin22@gmail.com>
2025-11-26 11:15:00 +0300

    (feat): compute costs during runtime

--- agent.py
+++ agent.py
@@ -1,9 +1,17 @@
 from datetime import datetime
+from enum import Enum
 from typing import List
 
 from spade.agent import Agent
 from spade.behaviour import PeriodicBehaviour
 from spade.message import Message
+
+
+class Cost(Enum):
+    MESSAGE_C = 1000
+    MESSAGE = 1
+    MEMORY = 0.1
+    OP = 0.01
 
 
 class ConsensusAgent(Agent):
@@ -21,17 +29,20 @@ class ConsensusAgent(Agent):
                 )
                 incoming_values.append(float(msg.body))  # type: ignore
                 msg = await self.receive()
+            self.agent.add_cost(Cost.MEMORY, len(incoming_values) + 1)
 
             # update internal value
             self.agent.value = self.agent.value + self.agent.alpha * sum(
                 [x_j - self.agent.value for x_j in incoming_values]
             )
+            self.agent.add_cost(Cost.OP, len(incoming_values) * 2 + 1)
 
             # publish the new value
             for recipient in self.agent.recipients:
                 message = Message(to=recipient)
                 message.body = f"{self.agent.value}"
                 await self.send(message)
+                self.agent.add_cost(Cost.MESSAGE)
 
             print(
                 f"agent {self.agent.jid} published the new value of {self.agent.value}"
@@ -49,16 +60,28 @@ class ConsensusAgent(Agent):
             )
 
     def __init__(
-        self, jid: str, value: float, recipients: List[str], start_at: datetime
+        self,
+        jid: str,
+        value: float,
+        recipients: List[str],
+        start_at: datetime,
+        is_reporter: bool = False,
+        epsilon: float = 1e-2,
     ):
         super().__init__(jid, "password", 5222, False)
 
+        self.is_reporter = is_reporter
         self.start_at = start_at
         self.value = value
         self.recipients = recipients
 
         self.alpha = 1 / (1 + len(recipients))
+        self.epsilon = epsilon
+        self.total_cost = 0
 
     async def setup(self):
         b = self.ConsensusBehaviour(period=1, start_at=self.start_at)
         self.add_behaviour(b)
+
+    def add_cost(self, cost: Cost, count: int = 1):
+        self.total_cost += cost.value * count
